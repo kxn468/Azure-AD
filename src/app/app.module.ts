bf39b3fb7ec434c653891c64b83cba72b0d11849
kxn468 <krishnasai.nallapaneni@sherwin.com>
2022-05-12 09:40:41 -0400

    updated app.modules.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,8 +35,8 @@ const isIE = window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigato
     AppRoutingModule,
     MsalModule.forRoot({    
       auth: {
-        clientId: '82a06437-82f8-4278-b151-1d1a96556105', // This is your client ID
-        authority: 'https://login.microsoftonline.com/eaf06ba5-c7d6-4142-8f78-c510fb1c832a', // This is your tenant ID
+        clientId: '03610c81-0914-444d-9bb3-01e1bb942202', // This is your client ID
+        authority: 'https://login.microsoftonline.com/b61a792f-e7f7-4975-8fbb-595e95fe6c33', // This is your tenant ID
         redirectUri: 'http://localhost:4200/',// This is your redirect URI
         postLogoutRedirectUri:'http://localhost:4200/'
       },     
@@ -57,7 +57,7 @@ const isIE = window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigato
       unprotectedResources: [],
       //export const protectedResourceMap:[string, string[]][]=[['https://localhost:44388/api/values', ['api://59b02905-8b6b-4665-a702-321e97392416/api-access']] ];
       protectedResourceMap: [
-        ['https://localhost:44313/', ['api://c597a277-4fab-498c-84ea-8e700dd0b9b1/api-access']]
+        ['https://localhost:44313/', ['api://03610c81-0914-444d-9bb3-01e1bb942202/api-access']]
       ],
       extraQueryParameters: {}
     })
